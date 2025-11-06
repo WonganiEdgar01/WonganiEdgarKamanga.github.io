4853fc1bc08c6c6f8c99fb16bc296544f69411a2
WonganiEdgar01 <wonganiedgar@gmail.com>
2025-11-06 23:26:59 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//typing animation script.js
+var typed = new Typed('.typing', {
+    strings: [" ", "Computer Engineer", "Web Developer", "UI/UX Designer", "Tech Enthusiast", "Photographer"], 
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true
+});
+
+var typed1 = new Typed('.typing1', {
+    strings: ["Wongani Edgar Kamanga"], 
+    typeSpeed: 100, 
+    backSpeed: 60,
+    loop: false
+});
